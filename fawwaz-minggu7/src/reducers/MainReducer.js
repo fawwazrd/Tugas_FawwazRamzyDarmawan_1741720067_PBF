9d2ff0584d16fa7cd54de0190c68adc3c32e43a0
fawwazrd <frayd1999@gmail.com>
2020-03-18 10:19:03 +0700

    praktikum7

---
+++ fawwaz-minggu7/src/reducers/MainReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux' 
+import todos from './TodoReducers'
+import visibilityFilter from './FilterReducer'
+export default combineReducers({ todos,
+visibilityFilter
+})
