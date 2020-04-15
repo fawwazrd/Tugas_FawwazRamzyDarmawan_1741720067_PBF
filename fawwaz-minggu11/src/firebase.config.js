bbc08f579d8933cf44be8fce9b55ab98dde8fc8b
fawwazrd <frayd1999@gmail.com>
2020-04-15 12:50:14 +0700

    tugas minggu 11

---
+++ fawwaz-minggu11/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyAkK1sN0xbQXerDre-77RUiZu1Wty4713w",
+  authDomain: "tryfirebasereact-af754.firebaseapp.com",
+  databaseURL: "https://tryfirebasereact-af754.firebaseio.com",
+  projectId: "tryfirebasereact-af754",
+  storageBucket: "tryfirebasereact-af754.appspot.com",
+  messagingSenderId: "706560422557",
+  appId: "1:706560422557:web:5090777e25bc1b8077da74",
+  measurementId: "G-C20P0G7F63"
+};
