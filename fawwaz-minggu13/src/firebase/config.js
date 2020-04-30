e9a091d992509bd56b4ca05dae12db89274b3121
fawwazrd <frayd1999@gmail.com>
2020-04-30 19:49:55 +0700

    minggu 13

---
+++ fawwaz-minggu13/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD64QG8RYrNxLipGrk1eiUvGsibOt_QNcs",
+    authDomain: "pbf12-efda5.firebaseapp.com",
+    databaseURL: "https://pbf12-efda5.firebaseio.com",
+    projectId: "pbf12-efda5",
+    storageBucket: "pbf12-efda5.appspot.com",
+    messagingSenderId: "514568010734",
+    appId: "1:514568010734:web:d78a4c7f52f102ada36cff",
+    measurementId: "G-J0XZ2LM0MS"
+  };
+
+  export default firebaseConfig;
